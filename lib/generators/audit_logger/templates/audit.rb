222a8a9cf2c8ff6e40bc16e4c5efd3b7e677f141
Vasin Dmitriy <vasindima779@gmail.com>
2015-05-22 11:44:41 +0300

    Change default audit.rb file

--- lib/generators/audit_logger/templates/audit.rb
+++ lib/generators/audit_logger/templates/audit.rb
@@ -1,9 +1,9 @@
 unless Rails.env.test?
   log_path_with_env = "#{Rails.root}/log/audit/#{Rails.env}"
 
-  ::ERROR_LOG        = AuditLogger::Audit.new("#{log_path_with_env}_error.log")
+  ::ERROR_LOG    = AuditLogger::Audit.new("#{log_path_with_env}_error.log")
 
-  ::PRODUCT_LOG      = AuditLogger::Audit.new("#{log_path_with_env}_product.log", timestamp: true, pid: true, severity: true)
-  ::CATEGORY_LOG     = AuditLogger::Audit.new("#{log_path_with_env}_category.log")
-  ::DB_MIGRATION_LOG = AuditLogger::Audit.new("#{log_path_with_env}_db_migration.log")
+  # ::AUDIT_NULL   = Audit::AuditLogger.new(File::NULL)
+  # ::AUDIT_STDOUT = Audit::AuditLogger.new(STDOUT)
+  # ::PRODUCT_LOG  = AuditLogger::Audit.new("#{log_path_with_env}_product.log", timestamp: true, pid: true, severity: true)
 end
